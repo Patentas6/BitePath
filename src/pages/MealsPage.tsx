a0de35f2ba661a4b6a2b04f6420f629cbbb20d85
[dyad] <git@dyad.sh>
2025-05-23 14:54:46 +0200

    [dyad] Changed My Meals page to row layout - wrote 1 file(s)

--- src/pages/MealsPage.tsx
+++ src/pages/MealsPage.tsx
@@ -21,7 +21,8 @@ const MealsPage = () => {
           </Button>
         </header>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {/* Changed from grid to vertical stacking with space */}
+        <div className="space-y-6">
           <MealForm />
           <MealList />
         </div>
